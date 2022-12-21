6cd0e3bcda63faff0dacb161e408fe8da6f75502
Viktor Raboshchuk <viktor.raboshchuk@nordstrom.com>
2022-12-21 15:33:16 +0200

    small syntax fix #2

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,7 +11,7 @@ const config: HardhatUserConfig = {
   networks: {
     mumbai: {
       url: process.env.STAGING_QUICK_NODE || "http://127.0.0.1",
-      accounts: [process.env.METAMASK_PRIVATE_KEYas string || "0x0000000000000000000000000000000000000000000000000000000000000000"],
+      accounts: [process.env.METAMASK_PRIVATE_KEY as string || "0x0000000000000000000000000000000000000000000000000000000000000000"],
     }
   },
   etherscan: {
